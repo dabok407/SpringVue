cb8434c6897dbd557da90f8028afb971c41385bb
dabok407 <dabok407@naver.com>
2020-03-13 17:17:02 +0900

    vue.js Basic Study Info Commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,98 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <form v-on:submit.prevent="saveForm">
+    <div>
+      <label for="username">id</label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw</label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+
+<script>
+
+    import axios from 'axios';
+
+    export default {
+        data: function () {
+            return{
+                username: ''
+                ,password: ''
+            }
+        }
+        ,methods: {
+            saveForm: function () {
+                // event.preventDefault(); // 새로고침 방지
+                // 상단form vue속성의 v-on:submit.prevent와 위의 preventDefault와 동일 기능
+                console.log("username : "+this.username+" / password : "+this.password);
+                var url = 'https://jsonplaceholder.typicode.com/users';
+                var data = {
+                    username : this.username
+                    ,password : this.password
+                }
+                axios.post(url, data)
+                    .then(function(response) {
+                        console.log(response);
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            }
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
+
+<!--
+
+<template>
+  <div>
+    <app-header
+      v-bind:propsdata="str"
+      v-bind:propsdata2="str2"
+      v-on:renew="renewStr"
+    >
+
+    </app-header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+
+    import AppHeader from './components/AppHeader.vue';
+
+    export default {
+        data: function () {
+            return {
+                str:'Header2'
+                ,str2:'Header3'
+            }
+        },components: {
+            'app-header': AppHeader
+        },methods: {
+            renewStr: function () {
+                this.str = 'hi!';
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+-->
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{propsdata}} / {{propsdata2}}</h1>
+    <button v-on:click="sendEvent">클릭</button>
+  </header>
+</template>
+
+<script>
+    export default {
+        props: ['propsdata', 'propsdata2']
+        ,methods: {
+            sendEvent: function () {
+                this.$emit('renew');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
